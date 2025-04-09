46ec29d25226393fea0c3cbb4f417dfc206644bc
sup-jun <1820246866@qq.com>
2025-04-09 23:05:29 +0800

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,8 +1,12 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -19,4 +23,12 @@ public interface DishMapper {
     //插入菜品数据
     @AutoFill(value = OperationType.INSERT)
     void insert(Dish dish);
+    //菜品分页查询
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
+    //根据主键查询菜品
+    @Select("select * from dish where id = #{id}")
+    Dish getById(Long id);
+    //根据主键删除菜品信息
+    @Delete("delete from dish where id = #{id}")
+    void deleteById(Long id);
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,8 +1,16 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+
+import java.util.List;
 
 public interface DishService {
     //新增菜品和对应的口味
     public void saveWithFlavor(DishDTO dishDTO);
+    //菜品分页查询
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+    //批量删除菜品
+    void deleteBatch(List<Long> ids);
 }
