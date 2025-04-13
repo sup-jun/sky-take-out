75b9cd3f3ac3766c0f8a508663f66a6419507214
sup-jun <1820246866@qq.com>
2025-04-13 22:21:57 +0800

    添加购物车业务代码开发

---
+++ sky-server/src/main/java/com/sky/service/ShoppingCartServicee.java
@@ -0,0 +1,9 @@
+package com.sky.service;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+public interface ShoppingCartServicee {
+    //添加购物车
+    void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+}
